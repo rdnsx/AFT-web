334467d784c8aa436e54538f2fc48df4f3631f88
Malte Gebremski <m.gebremski@steinberg.de>
2023-04-08 20:58:14 +0200

    Change st.cache_data

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title='Aquarium Fertilizer Tracker', page_icon=':bar_chart:', layout='wide')
 
-@st.cache
+@st.cache_data
 def load_data():
     return pd.read_csv('/app/data/aft.csv')
 
