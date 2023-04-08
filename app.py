4e7a0841a43d3257eceb07fee7d07d7950dc021a
Malte Gebremski <m.gebremski@steinberg.de>
2023-04-08 20:52:52 +0200

    Update csv path

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='My CSV Viewer', page_icon=':bar_chart:', layout='wide')
+st.set_page_config(page_title='Aquarium Fertilizer Tracker', page_icon=':bar_chart:', layout='wide')
 
 @st.cache
 def load_data():
-    return pd.read_csv('/app/data/data.csv')
+    return pd.read_csv('/app/data/aft.csv')
 
 def main():
-    st.title('My CSV Viewer')
+    st.title('Aquarium Fertilizer Tracker')
     data = load_data()
     st.dataframe(data)
 
